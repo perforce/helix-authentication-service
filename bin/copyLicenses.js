ec1a975c8f43f811944c5a2372ef77c4b217d862
Nathan Fiedler <nfiedler@perforce.com>
2019-08-23 16:26:43 -0700

    Add script to collect third party licenses
    Only considers the direct dependencies, which should be sufficient.
    Copies the license files into the docs/licenses directory, much like Swarm
    does for the React-related dependencies.

---
+++ bin/copyLicenses.js
@@ -0,0 +1,51 @@
+//
+// For each production module, find its license file and copy it into the
+// 'docs/licenses' directory for distribution. Only considers those packages
+// that are direct dependencies of this module.
+//
+const checker = require('license-checker')
+const fs = require('fs')
+const path = require('path')
+
+// collect our direct dependencies into a unique set of package names
+const packageJson = require('../package.json')
+const directPackages = new Set()
+for (const pkg in packageJson.dependencies) {
+  directPackages.add(pkg)
+}
+
+// use the license-checker to gather all of the available data
+checker.init({
+  start: '.'
+}, function (err, packages) {
+  if (err) {
+    console.error(err)
+  } else {
+    for (const p in packages) {
+      let pkgName = p.replace(/@.*/, '')
+      if (pkgName === '') {
+        pkgName = p.substring(1).replace(/@.*/, '')
+      }
+      if (directPackages.has(pkgName)) {
+        // The same package may appear more than once, with different versions.
+        // Only one will be considered, and it is likely the oldest version. In
+        // practice it should not matter all that much.
+        directPackages.delete(pkgName)
+        const licenseFile = packages[p].licenseFile
+        if (licenseFile) {
+          const leaf = path.basename(licenseFile)
+          const dir = path.basename(path.dirname(licenseFile))
+          const dest = 'docs/licenses/' + dir + '/' + leaf
+          if (!fs.existsSync(path.dirname(dest))) {
+            fs.mkdirSync(path.dirname(dest), { recursive: true, mode: 0o755 })
+          }
+          if (fs.existsSync(path)) {
+            fs.unlinkSync(path)
+          }
+          const data = fs.readFileSync(licenseFile, 'utf8')
+          fs.appendFileSync(dest, data, 'utf8', '0o644', 'a')
+        }
+      }
+    }
+  }
+})
