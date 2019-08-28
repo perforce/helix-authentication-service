027db96298cc5323cdfeae7ae8dc1de4b7577a56
Nathan Fiedler <nfiedler@perforce.com>
2019-08-28 14:55:16 -0700

    Add a sticky footer with copyright and link to perforce.com

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const router = express.Router()
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index')
+  // detect if the system has not yet been configured by an administrator
+  const showHelp = (process.env.OIDC_ISSUER_URI === undefined && process.env.SAML_IDP_SSO_URL === undefined)
+  res.render('index', { showHelp })
 })
 
 module.exports = router
