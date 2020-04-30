78739ec518badaa349efeb336abacaf81481b4a7
Nathan Fiedler <nfiedler@perforce.com>
2020-04-30 15:32:42 -0700

    Refactor to increase coverage of automated tests
    Discovered that without certain environment variables being defined, the
    service would start in a non-fucntional state. That has been fixed, and to
    ensure this behavior stays consistent, the code has been refactored to
    increase the coverage of the automated tests.
    
    No user-visible changes otherwise.
    
    npm test passes

---
+++ test/server.test.js
@@ -0,0 +1,69 @@
+//
+// Copyright 2020 Perforce Software
+//
+const { assert } = require('chai')
+const { describe, it } = require('mocha')
+const sut = require('@lib/server')
+
+describe('Server basics', function () {
+  describe('getPort', function () {
+    it('should default to 3000', function () {
+      assert.equal(sut.getPort({}), '3000')
+    })
+
+    it('should prefer PORT over SVC_BASE_URI', function () {
+      const env = {
+        PORT: '3001',
+        SVC_BASE_URI: 'http://localhost:3002'
+      }
+      assert.equal(sut.getPort(env), '3001')
+    })
+
+    it('should use port in SVC_BASE_URI', function () {
+      assert.equal(sut.getPort({ SVC_BASE_URI: 'http://localhost:3003' }), '3003')
+      assert.equal(sut.getPort({ SVC_BASE_URI: 'https://localhost:3004' }), '3004')
+      assert.equal(sut.getPort({ SVC_BASE_URI: 'http://localhost' }), '80')
+      assert.equal(sut.getPort({ SVC_BASE_URI: 'https://localhost' }), '443')
+    })
+  })
+
+  describe('normalizePort', function () {
+    it('should normalize the port value', function () {
+      assert.equal(sut.normalizePort('abc'), 'abc')
+      assert.equal(sut.normalizePort('3000'), 3000)
+      assert.equal(sut.normalizePort('-8'), false)
+    })
+  })
+
+  describe('getProtocol', function () {
+    it('should use PROTOCOL if defined', function () {
+      assert.equal(sut.getProtocol({ PROTOCOL: 'smtp' }), 'smtp:')
+    })
+
+    it('should get protocol from SVC_BASE_URI', function () {
+      assert.equal(sut.getProtocol({ SVC_BASE_URI: 'http://localhost' }), 'http:')
+    })
+
+    it('should default to http:', function () {
+      assert.equal(sut.getProtocol({}), 'http:')
+    })
+  })
+
+  describe('getServiceURI', function () {
+    it('should use SVC_BASE_URI if defined', function () {
+      assert.equal(sut.getServiceURI({ SVC_BASE_URI: 'https://remotehost:8080' }), 'https://remotehost:8080')
+    })
+
+    it('should use PROTOCOL if defined', function () {
+      assert.equal(sut.getServiceURI({ PROTOCOL: 'smtp' }), 'smtp://localhost:3000')
+    })
+
+    it('should use PORT if defined', function () {
+      assert.equal(sut.getServiceURI({ PORT: '3001' }), 'http://localhost:3001')
+    })
+
+    it('should default to http on port 3000', function () {
+      assert.equal(sut.getServiceURI({}), 'http://localhost:3000')
+    })
+  })
+})
