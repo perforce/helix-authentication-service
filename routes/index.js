9f09b2178499c062c192f31c85220ef9a0ff4cdb
Nathan Fiedler <nfiedler@perforce.com>
2020-09-01 16:19:14 -0700

    build: introduce use of eslint to catch JS issues
    ESLint (https://eslint.org/) is a JavaScript static analysis tool which
    finds many coding problems that could become issues at run time. For now,
    the tool is installed as a dev dependency and can be run like so:
    
    $ npx eslint lib routes
    
    Added several line-disables for unused variables, which is unavoidable for
    callbacks whose definition is outside of our control.
    
    npm test passes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
+// eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
   // detect if the system has not yet been configured by an administrator
   const showHelp = (process.env.OIDC_ISSUER_URI === undefined && process.env.SAML_IDP_SSO_URL === undefined)
