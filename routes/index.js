667befb865549df4a9a72ee5f5b8605a559ac6f9
Nathan Fiedler <nfiedler@perforce.com>
2021-03-01 10:34:03 -0800

    feat: add SAML_IDP_METADATA_FILE config option
    Instead of using SAML_IDP_METADATA_URL, the admin can provide the path to an
    XML file for the IdP metadata, using the new setting SAML_IDP_METADATA_FILE.
    This is useful if the IdP does not provide a URL for its metadata.
    
    npm test passes

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router()
 // eslint-disable-next-line no-unused-vars
 router.get('/', (req, res, next) => {
   // detect if the system has not yet been configured by an administrator
-  const hasSaml = (process.env.SAML_IDP_METADATA_URL || process.env.SAML_IDP_SSO_URL)
+  const hasSaml = (process.env.SAML_IDP_METADATA_FILE || process.env.SAML_IDP_METADATA_URL || process.env.SAML_IDP_SSO_URL)
   const showHelp = (process.env.OIDC_ISSUER_URI === undefined && !hasSaml)
   res.render('index', { showHelp })
 })
