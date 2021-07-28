ffa4af099ae546955a7ee6ec42913f9893faaff7
Nathan Fiedler <nfiedler@perforce.com>
2021-07-28 15:05:02 -0700

    test: add unit test for Swarm SAML validation
    The new test is only the HAS side of the process, with the unit test making
    the requests that Swarm would make. Tests the validation of the SAML
    response sent from HAS to Swarm, then to p4d, and finally back to HAS again.
    
    Added saml2-js dev dependency.
    
    npm test passes

--- test/saml.test.js
+++ test/saml.test.js
@@ -3,10 +3,12 @@
 //
 const fs = require('fs')
 const https = require('https')
+const querystring = require('querystring')
 const { assert } = require('chai')
 const { after, before, describe, it } = require('mocha')
 const { Builder, By, Capabilities, until } = require('selenium-webdriver')
 const { Options } = require('selenium-webdriver/firefox')
+const saml2 = require('saml2-js')
 const { getRequestId } = require('./helpers')
 const path = require('path')
 global.include = (p) => require(path.join(__dirname, '..', p))
@@ -17,7 +19,7 @@ global.include = (p) => require(path.join(__dirname, '..', p))
 // Take a screenshot:
 //
 // const imagestr = await driver.takeScreenshot()
-// fs.writeFileSync('screenshot.b64', imagestr)
+// fs.writeFileSync('screenshot.png', imagestr, 'base64')
 //
 
 describe('SAML authentication', function () {
@@ -158,6 +160,95 @@ describe('SAML authentication', function () {
       By.xpath('//section[contains(@class, "Site-content")]/div/h1')))
     const h1Text = await h1Elem.getText()
     assert.include(h1Text, 'Logout successful')
+  })
+
+  it('should validate SAML response via 1-step', async function () {
+    this.timeout(30000)
+    // Prepare the test service provider details to get the login URL using the
+    // current docker setup which expects Swarm to be using certain details.
+    const spOptions = {
+      entity_id: 'urn:swarm-example:sp',
+      certificate: fs.readFileSync('test/client.crt', 'utf-8'),
+      private_key: fs.readFileSync('test/client.key', 'utf-8'),
+      assert_endpoint: 'https://swarm.doc:8043/api/v10/session',
+      auth_context: {
+        comparison: 'exact',
+        class_refs: [
+          'urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport'
+        ]
+      },
+      nameid_format: 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress',
+      sign_get_request: false,
+      allow_unencrypted_assertion: true
+    }
+    const sp = new saml2.ServiceProvider(spOptions)
+    const idpOptions = {
+      sso_login_url: 'https://authen.doc/saml/login',
+      sso_logout_url: 'https://authen.doc/saml/logout',
+      certificates: [fs.readFileSync('certs/server.crt', 'utf-8')],
+      sign_get_request: false,
+      allow_unencrypted_assertion: true
+    }
+    const idp = new saml2.IdentityProvider(idpOptions)
+    // begin the 1-step login process to get the actual login URL
+    const loginUrl = await new Promise((resolve, reject) => {
+      sp.create_login_request_url(idp, {}, (err, loginUrl, _requestId) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(loginUrl)
+        }
+      })
+    })
+    // Use the browser to perform the authenticaton since the process relies on
+    // the browser cookies to track the session.
+    await driver.get(loginUrl)
+    // Seems like the browser and/or service remembers the authenticated
+    // session? Well, whatever works.
+    await driver.wait(until.urlContains('authen.doc'), 10000)
+    const responseForm = await driver.wait(until.elementLocated(By.css('form')))
+    const responseInput = await responseForm.findElement(By.name('SAMLResponse'))
+    // extract the SAML response from the form and send to validate endpoint
+    const samlResponse = await responseInput.getAttribute('value')
+    // send SAML response to /validate with POST for validation
+    await new Promise((resolve, reject) => {
+      const cert = fs.readFileSync('test/client.crt')
+      const key = fs.readFileSync('test/client.key')
+      const postData = querystring.stringify({
+        SAMLResponse: samlResponse
+      })
+      const req = https.request({
+        hostname: 'authen.doc',
+        path: '/saml/validate',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Length': Buffer.byteLength(postData)
+        },
+        rejectUnauthorized: false,
+        requestCert: false,
+        agent: false,
+        key,
+        cert
+      }, (res) => {
+        assert.equal(res.statusCode, 200)
+        assert.match(res.headers['content-type'], /^application\/json/)
+        res.setEncoding('utf-8')
+        let data = ''
+        res.on('data', (chunk) => { data += chunk })
+        res.on('end', () => {
+          const json = JSON.parse(data)
+          assert.equal(json.nameID, 'saml.jackson@example.com')
+          assert.equal(json.nameIDFormat, 'urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress')
+          assert.exists(json.sessionIndex)
+          resolve()
+        })
+      }).on('error', (err) => {
+        reject(err)
+      })
+      req.write(postData)
+      req.end()
+    })
   })
 
   it('should return valid SAML metadata', function (done) {
