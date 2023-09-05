a54bae7ca213f2b2a7139ae410adeff528a7810f
Nathan Fiedler <nfiedler@perforce.com>
2023-09-05 11:21:22 -0700

    build: fix Linux package build process in docker
    The `docker run` command to extract the build artifact started failing
    intermittently, resulting in apparent package signing issues. Now the
    process creates an image based on `scratch` that contains only the build
    artifacts, and then we use the `--output` flag to `docker build` to write
    the results to the local disk rather than the build image.

--- build/build_centos8.sh
+++ build/build_centos8.sh
@@ -3,11 +3,9 @@
 # Build package tarball for CentOS 8.
 #
 set -e
-docker build -f build/centos8/Dockerfile --tag auth-svc:centos8 .
 echo 'creating tar file...'
 rm -rf yum
 mkdir -p yum/rhel/8/x86_64
-RPMPATH=$(docker run --rm --entrypoint find auth-svc:centos8 /root/rpmbuild/RPMS -name '*.rpm')
-docker run --rm --entrypoint cat auth-svc:centos8 ${RPMPATH} > yum/rhel/8/x86_64/helix-auth-svc-snapshot.rpm
+docker build -f build/centos8/Dockerfile --output type=local,dest=yum/rhel/8/x86_64 .
 tar zcf helix-auth-svc-centos8.tgz yum
 rm -rf yum
--- build/build_ubuntu22.sh
+++ build/build_ubuntu22.sh
@@ -3,11 +3,9 @@
 # Build package tarball for Ubuntu 22.
 #
 set -e
-docker build -f build/ubuntu22/Dockerfile --tag auth-svc:ubuntu22 .
 echo 'creating tar file...'
 rm -rf apt
 mkdir -p apt/ubuntu/jammy/incoming
-DEBPATH=$(docker run --rm --entrypoint find auth-svc:ubuntu22 /build -name '*.deb')
-docker run --rm --entrypoint cat auth-svc:ubuntu22 ${DEBPATH} > apt/ubuntu/jammy/incoming/helix-auth-svc_snapshot.deb
+docker build -f build/ubuntu22/Dockerfile --output type=local,dest=apt/ubuntu/jammy/incoming .
 tar zcf helix-auth-svc-ubuntu22.tgz apt
 rm -rf apt
