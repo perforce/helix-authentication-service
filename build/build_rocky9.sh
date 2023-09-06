d12f5d01e288c8cb8f9f9b6f858bd825b37d8764
Nathan Fiedler <nfiedler@perforce.com>
2023-09-06 16:23:01 -0700

    build: correct the build directory name

--- build/build_rocky9.sh
+++ build/build_rocky9.sh
@@ -6,6 +6,6 @@ set -e
 echo 'creating tar file...'
 rm -rf yum
 mkdir -p yum/rhel/9/x86_64
-docker build -f build/rocky9/Dockerfile --output type=local,dest=yum/rhel/9/x86_64 .
+docker build -f build/centos9/Dockerfile --output type=local,dest=yum/rhel/9/x86_64 .
 tar zcf helix-auth-svc-centos9.tgz yum
 rm -rf yum
