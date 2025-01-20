8f3b09e6065369848881091d5100b8b8e43c7276
Nathan Fiedler <nfiedler@perforce.com>
2025-01-20 12:54:51 -0800

    test: rely on Swarm SSO integration for testing

--- containers/auth-svc/saml_idp.conf.cjs
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'urn:swarm-example:sp': {
-    // undoc: use regex to allow for swarm with multiple p4d instances
-    acsUrlRe: 'https://swarm\\.doc:8043/[^/]+/api/v10/session'
-  }
-}
