338d99b5780fada312112e83be25ea9fc1830b3f
Nathan Fiedler <nfiedler@perforce.com>
2025-01-27 14:55:31 -0800

    test: need this file for automated tests

---
+++ containers/auth-svc/saml_idp.conf.cjs
@@ -0,0 +1,9 @@
+//
+// DO NOT REMOVE, USED BY AUTOMATED TESTS
+//
+module.exports = {
+  'urn:swarm-example:sp': {
+    // undoc: use regex to allow for swarm with multiple p4d instances
+    acsUrlRe: 'https://swarm\\.doc:8043/[^/]+/api/v10/session'
+  }
+}
